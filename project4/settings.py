258c3cc32664f3ec3f2fc19828e5213033286cb3
Melnyk Vadym <vadym.melnyk@wp.pl>
2021-12-03 22:54:35 +0100

    heroku11

--- project4/settings.py
+++ project4/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://vadayi.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['https://vadaiy.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
